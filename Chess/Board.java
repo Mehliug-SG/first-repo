c760c5d04514bb2c8c76b1567c89f245155f3c07
Guilhem <gsaintgaudin@gmail.com>
2023-03-27 19:38:45 +0200

    advancements on the Piece enumeration, debuted the Board class

---
+++ Chess/Board.java
@@ -0,0 +1,13 @@
+package Chess;
+
+import java.util.ArrayList;
+
+public class Board {
+
+    private ArrayList<ArrayList<Piece>> board;
+
+    public Board(){
+        this.board = new ArrayList<ArrayList<Piece>>();
+    }
+
+}
--- Chess/Piece.java
+++ Chess/Piece.java
@@ -1,4 +1,19 @@
 package Chess;
 
+import java.util.ArrayList;
+
 public enum Piece {
+    PAWN(true),
+    ROOK(true),
+    KNIGHT(false),
+    BISHOP(false),
+    QUEEN(false),
+    KING(true);
+
+    private boolean untouched;
+
+    Piece(boolean untouched) {
+        this.untouched = untouched;
+    }
+
 }
